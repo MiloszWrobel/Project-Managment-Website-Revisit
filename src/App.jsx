8f442a3d3187f12f0e2e8cfb8daad1e409c09405
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-30 11:40:54 +0000

    Removed Main section added more functionality to the app comonent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Sidebar from "./components/Sidebar";
-import MainSection from "./components/MainSection";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
+import Project from "./components/Project";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: false,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: !prevState.selectedProjectId,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === true) {
+    content = <Project handleClick={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === false) {
+    content = <NoProjectSelected handleClick={handleAddProject} />;
+  }
+
   return (
     <main className="flex h-screen gap-8">
-      <Sidebar />
-      <MainSection />
+      <Sidebar handleClick={handleAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
-export default function Sidebar() {
+export default function Sidebar({ handleClick }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-3xl mt-8">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <button className="w-auto text-left px-4 py-2 rounded-lg my-1 bg-stone-800 hover:text-stone-200 hover:bg-stone-600">
+        <button
+          className="w-auto text-left px-4 py-2 rounded-lg my-1 bg-stone-800 hover:text-stone-200 hover:bg-stone-600"
+          onClick={handleClick}
+        >
           + Add Project
         </button>
       </div>
